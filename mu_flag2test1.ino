cfdaf2a437203d4a61b37e647ef96434f15cade1
SmrithiNarayanan <smr330@gmail.com>
2023-12-07 14:50:10 +0100

    Add files via upload
    addition of flag 2

---
+++ mu_flag2test1.ino
@@ -0,0 +1,147 @@
+#define TRIGGER_PIN 11
+#define ECHO_PIN 12
+#define USONIC_DIV 58.0
+#define MEASURE_SAMPLE_DELAY 5
+#define MEASURE_SAMPLES 20
+#define MEASURE_DELAY 25
+
+// Motor A
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+// Motor B
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+//flag variables
+int flag=0;
+
+void setup(){
+Serial.begin(9600); // Serial monitoring
+pinMode(TRIGGER_PIN, OUTPUT); // Initializing Trigger Output and Echo Input
+pinMode(ECHO_PIN, INPUT);
+digitalWrite(TRIGGER_PIN, LOW); // Reset the trigger pin and wait a half a second
+delayMicroseconds(10000);
+
+// Set all the motor control pins to outputs
+pinMode(enA, OUTPUT);
+pinMode(enB, OUTPUT);
+pinMode(in1, OUTPUT);
+pinMode(in2, OUTPUT);
+pinMode(in3, OUTPUT);
+pinMode(in4, OUTPUT);
+}
+
+void loop(){
+
+if (flag==0) {
+  // Turn on motor A
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 200); // Set speed to 200 out of possible range 0~255
+  // Turn on motor B
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW); 
+  analogWrite(enB, 200); // Set speed to 200 out of possible range 0~255
+  
+  
+  //delay(MEASURE_DELAY);
+  long distance = measure()+22;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" mm");
+
+  // Check if the distance value is 200 four times
+  static int counter_0 = 0;
+  if (distance <= 300) {
+    counter_0++;
+    if (counter_0 == 4) {
+      flag=1;
+      counter_0 = 0;
+      Serial.println("flag=1");
+    }
+  } else {
+    counter = 0;
+  }
+  delay(1000);
+}
+if (flag==1){
+// move motor forward small amount 
+//Turn 90 degrees
+static int counter_1 = 0;
+// check distance 4 times 
+for(i=0;i<5;i++){
+  //Start sensor detection again
+  long distance = measure()+22;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" mm");
+  //increment counter 
+  if (distance > 300) {
+    counter_1++; 
+}
+}
+if(counter_1>=2){
+  //move forward a small amount (test)
+  //then turn 90 degrees again
+  flag = 0;
+}
+if(counter_1<2){
+  flag=2;
+}
+}
+if(flag==2){
+  //move motor small amount
+  //turn 90 degrees
+  //start continuous motor movement 
+  // check distance
+  long distance = measure()+22;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" mm");
+
+  // Check if the distance value is 200 four times
+  static int counter_0 = 0;
+  if (distance <= 300) {
+    counter_0++;
+    if (counter_0 == 4) {
+      //Move small amount forward
+    }
+  } else {
+    counter = 0;
+  }
+  delay(1000);
+}
+}
+void switchMotorDirection() {
+  // Turn on motor A in opposite direction
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, 200); // Set speed to 200 out of possible range 0~255
+  // Turn on motor B in opposite direction
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH); 
+  analogWrite(enB, 200); // Set speed to 200 out of possible range 0~255
+  delay(4000);
+}
+
+long measure(){
+long measureSum = 0;
+for (int i = 0; i < MEASURE_SAMPLES; i++){
+delay(MEASURE_SAMPLE_DELAY);
+measureSum += singleMeasurement();
+}
+return measureSum / MEASURE_SAMPLES;
+}
+
+long singleMeasurement(){
+long duration = 0; // Measure: Put up Trigger...
+digitalWrite(TRIGGER_PIN, HIGH); // Wait for 11 µs ...
+delayMicroseconds(20); // Put the trigger down ...
+digitalWrite(TRIGGER_PIN, LOW); // Wait for the echo ...
+duration = pulseIn(ECHO_PIN, HIGH);
+return (long) (((float) duration / USONIC_DIV) * 10.0);
+}
