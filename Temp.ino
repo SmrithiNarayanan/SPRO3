2e6fc2cb6eec4f2fdfae87ffc7056705061258dc
scdiez <70489460+scdiez@users.noreply.github.com>
2023-12-15 13:46:20 +0100

    Update Temp.ino

--- Temp.ino
+++ Temp.ino
@@ -1,32 +1,30 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS 9
 
-OneWire oneWire(ONE_WIRE_BUS);
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);	
+
+// Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
-void setup() {
-  // Start the serial communication
+void setup(void)
+{
+  sensors.begin();	// Start up the library
   Serial.begin(9600);
-  // Start the DS18B20 sensor
- sensors.begin();
 }
-void loop() {
-  // Request temperature data from all sensors
-  sensors.requestTemperatures();
-  // Get the temperature in Celsius
-  float temperatureC = sensors.getTempCByIndex(0);
-  // Check if the temperature reading is valid
-  if (temperatureC != DEVICE_DISCONNECTED_C) {
-    // Print the temperature to the Serial Monitor
-    Serial.print("Temperature: ");
-    Serial.print(temperatureC);
-    Serial.println(" °C");
-  } else {
-    Serial.println("Error reading temperature");
-  }
 
-  // Wait for a moment before taking the next reading
-  delay(1000);
+void loop(void)
+{ 
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println (" C  ");
+  
+  delay(500);
 }
